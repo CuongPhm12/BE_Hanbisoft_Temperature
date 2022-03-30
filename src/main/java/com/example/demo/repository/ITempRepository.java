7f77d3d3f76bf1f994ff557e552b590b8dd4c4e9
liemnguyen <zovanser@gmail.com>
2022-03-30 08:46:41 +0700

    commit 30.3

--- src/main/java/com/example/demo/repository/ITempRepository.java
+++ src/main/java/com/example/demo/repository/ITempRepository.java
@@ -6,8 +6,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ITempRepository extends JpaRepository<Temperature,Long> {
     Page<Temperature> findAll(Pageable pageable);
+
 //    Page<Temperature> findAllByUser_id(Long id);
 }
